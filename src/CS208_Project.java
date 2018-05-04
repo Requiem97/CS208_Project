35c089074b116b988cb29520c35df6700656ae9e
Requiem97 <marius.1997@gmail.com>
2018-05-04 22:31:07 +0800

    formatted code

--- src/CS208_Project.java
+++ src/CS208_Project.java
@@ -59,60 +59,59 @@ public class CS208_Project {
 		default:
 			System.out.println("Invalid Input");
 		}
-		
+
 	}
-	
+
 	public static void Bresenham(double x0, double y0, double x1, double y1) {
 		double dx = x1 - x0;
 		double dy = y1 - y0;
-		double dy2 = 2*dy;
-		double dx2 = 2*dx;
+		double dy2 = 2 * dy;
+		double dx2 = 2 * dx;
 		double err;
 		double p;
 		double[] x;
 		double[] y;
 		int i;
-		if(dx > dy) {
+		if (dx > dy) {
 			p = dy2 - dx;
-			err= dy2 - (2*dx);;
-			x=new double[(int)Math.round(Math.abs(dx))+1];
-			y=new double[(int)Math.round(Math.abs(dx))+1];
-			x[0]=x0;
-			y[0]=y0;
+			err = dy2 - (2 * dx);
+			;
+			x = new double[(int) Math.round(Math.abs(dx)) + 1];
+			y = new double[(int) Math.round(Math.abs(dx)) + 1];
+			x[0] = x0;
+			y[0] = y0;
 			System.out.println("(" + x[0] + ", " + y[0] + ")");
-			for (i = 0;i < dx;i++) {
-				if(p < 0) {
-					x[i+1] = x[i]+1;
-					y[i+1] = y[i];
+			for (i = 0; i < dx; i++) {
+				if (p < 0) {
+					x[i + 1] = x[i] + 1;
+					y[i + 1] = y[i];
 					p = p + dy2;
-				}
-				else {
-					x[i+1] = x[i]+1;
-					y[i+1] = y[i]+1;
+				} else {
+					x[i + 1] = x[i] + 1;
+					y[i + 1] = y[i] + 1;
 					p = p + err;
 				}
-				System.out.println("(" + x[i+1] + ", " + y[i+1] + ")");
+				System.out.println("(" + x[i + 1] + ", " + y[i + 1] + ")");
 			}
-		}else {
+		} else {
 			p = dx2 - dy;
-			err= dx2 - (2*dy);
-			x=new double[(int)Math.round(Math.abs(dy))+1];
-			y=new double[(int)Math.round(Math.abs(dy))+1];
-			x[0]=x0;
-			y[0]=y0;
+			err = dx2 - (2 * dy);
+			x = new double[(int) Math.round(Math.abs(dy)) + 1];
+			y = new double[(int) Math.round(Math.abs(dy)) + 1];
+			x[0] = x0;
+			y[0] = y0;
 			System.out.println("(" + x[0] + ", " + y[0] + ")");
-			for (i = 0;i < dy;i++) {
-				if(p < 0) {
-					y[i+1] = y[i]+1;
-					x[i+1] = x[i];
-					p = p+ dx2;
+			for (i = 0; i < dy; i++) {
+				if (p < 0) {
+					y[i + 1] = y[i] + 1;
+					x[i + 1] = x[i];
+					p = p + dx2;
+				} else {
+					y[i + 1] = y[i] + 1;
+					x[i + 1] = x[i] + 1;
+					p = p + err;
 				}
-				else {
-					y[i+1] = y[i]+1;
-					x[i+1] = x[i]+1;
-					p = p+err;
-				}
-				System.out.println("(" + x[i+1] + ", " + y[i+1] + ")");
+				System.out.println("(" + x[i + 1] + ", " + y[i + 1] + ")");
 			}
 		}
 		Plot2DPanel plot = new Plot2DPanel();
@@ -121,9 +120,8 @@ public class CS208_Project {
 		frame.setSize(848, 480);
 		frame.setContentPane(plot);
 		frame.setVisible(true);
-		
-	}
 
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
